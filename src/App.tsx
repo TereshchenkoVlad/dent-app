691e720d6df6dfca76468c69d9a671f2c443e409
Vladyslav Tereshchenko <vladyslav.tereshchenko@meliorgames.com>
2021-11-26 21:24:22 +0200

    Add app config file and absolute path

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React from "react";
-import Header from "./components/Header";
+
+import Header from "components/Header";
+import { AppConfig } from "config/applicationConfig";
 
 function App() {
   return (
     <div>
       <Header />
+      <iframe
+        width="100%"
+        height="528"
+        loading="lazy"
+        title="lyashchuk"
+        style={{ border: 0 }}
+        src={AppConfig.google_api}
+      />
     </div>
   );
 }
---
+++ src/config/applicationConfig.ts
@@ -0,0 +1,4 @@
+export const AppConfig = {
+  google_api:
+    "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d10172.766216026877!2d30.3788098!3d50.4002676!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x3d4d7b0d972f9adf!2z0KHRgtC-0LzQsNGC0L7Qu9C-0LPRltGPINC70ZbQutCw0YDRjyDQm9GP0YjRg9C60LAsINCW0Log0KHQvtGE0ZbRjw!5e0!3m2!1suk!2sua!4v1637953610830!5m2!1suk!2sua",
+};
